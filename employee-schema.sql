31be352c074cfb07215be5aca9744e76115bb296
Justin Byrd <justin_byrd@hotmail.com>
2021-05-04 16:06:42 -0400

    Fixed manager_id in employee table

--- employee-schema.sql
+++ employee-schema.sql
@@ -24,8 +24,8 @@ CREATE TABLE department (
 	 first_name VARCHAR(30),
 	 last_name VARCHAR(30),
 	 role_id INT,
-	 department_id INT,
+	 manager_id INT,
 	 PRIMARY KEY (id),
 	 FOREIGN KEY (role_id) REFERENCES role(id),
-	 FOREIGN KEY (department_id) REFERENCES department(id)
+	 FOREIGN KEY (manager_id) REFERENCES employee(id)
  )
